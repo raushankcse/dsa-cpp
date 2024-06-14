b94a48e584a95a443d1cb790bdb559beaebee178
Raushan Kumar <raushankcse@gmail.com>
2024-06-14 16:16:10 +0530

    untrack

---
+++ recursion/recursion6.cpp
@@ -0,0 +1,24 @@
+// print names n times using recursion
+
+// #include<iostream>
+// using namespace std;
+
+// void print(string name, int n){
+//   if(n==0) return;
+//   cout<<name<<endl;
+
+//   return print(name, n-1);
+// }
+
+
+// int main(){
+
+//   string name="raushan";
+//   int n;
+//   cout<<"enter n: ";
+//   cin>>n;
+//   print(name, n);
+//   return 0;
+// }
+
+
