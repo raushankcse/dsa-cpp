771b884b45254f02f2b730e0dc773c2dd9d562ba
Raushan Kumar <raushankcse@gmail.com>
2024-06-20 08:08:51 +0530

    recrsion reverse

---
+++ recursion/recursion9.cpp
@@ -0,0 +1,25 @@
+// sum of 1 to n numbers
+
+// #include<iostream>
+// using namespace std;
+
+// int sumN=0;
+// int sum(int n)
+// {
+//   if(n==0) return 0;
+//   sumN = sumN +n;
+//   sum(n-1);
+//   return sumN;
+// }
+
+
+// int main()
+// {
+//   int n;
+//   cout<<"enter n: ";
+//   cin>>n;
+//   int total = sum(n);
+//   cout<<total<<endl;
+// }
+
+
