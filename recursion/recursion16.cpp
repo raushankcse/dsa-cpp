e1014a7a875e8209f58aee1fc0df2ef61a32989c
Raushan Kumar <raushankcse@gmail.com>
2024-07-06 15:48:27 +0530

    sum of n series

---
+++ recursion/recursion16.cpp
@@ -0,0 +1,22 @@
+// sum of n series
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int n)
+{
+  if(n==0)
+  {
+    return 0;
+  }
+
+  return n + sum(n-1);
+}
+
+int main()
+{
+
+  int n= 3;
+  cout<<sum(3);
+  return 0;
+}
